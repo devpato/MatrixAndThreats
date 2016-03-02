32d7b62bd56f46395ce1dc4038a87997266b4cbe
Patricio Vargas <pevargas@valdosta.edu>
2016-03-01 20:02:10 -0500

    file scanner set up

--- MatrixThreats.java
+++ MatrixThreats.java
@@ -7,7 +7,12 @@ import java.util*;
 
 public class MatrixThreats{
     public static void main(String[] args){
-        
+        int matrix_A[][],matrix_B[][],threats[][];
+        String fName = "";
+        Scanner scan = new Scanner (System.in);
+        System.out.println("File name:");
+        fName = scan.next();
+        scan.close();
     }
     
     
