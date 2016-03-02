471e19ea62920a4e3a1202c5835257cff6228b3c
Patricio Vargas <pevargas@valdosta.edu>
2016-03-01 20:15:49 -0500

    dimensions validator

--- MatrixThreats.java
+++ MatrixThreats.java
@@ -7,13 +7,29 @@ import java.util*;
 
 public class MatrixThreats{
     public static void main(String[] args){
+        boolean flag = false;
         int matrix_A[][],matrix_B[][],threats[][];
+        String xvalues = "";
+        int [] values;
         String fName = "";
+        String space= "\\s+";
         Scanner scan = new Scanner (System.in);
         System.out.println("File name:");
         fName = scan.next();
         scan.close();
-    }
-    
-    
+        try{
+            BufferedReader buffer = new BufferedReader(new FileReader(fName));
+            xvalues = buffer.readLine();
+            values = MatrixThreats.stringArrayToIntArray(xvalues.split(space));
+            if(values[0]==values[3]){
+                flag = true;
+            }
+            else{
+                buffer.close();
+                System.out.println("The dimensions of your array are wrong");
+                return;
+            }
+            
+        }
+    }     
 }
