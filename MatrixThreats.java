c58d1ae8c98f01ad8c1bc434128055e992e7ccb3
Patricio Vargas <pevargas@valdosta.edu>
2016-03-01 20:28:15 -0500

    grabing values from first line

--- MatrixThreats.java
+++ MatrixThreats.java
@@ -2,25 +2,32 @@
 // OS Spring 2016
 //Valdosta State University
 
-import java.io*;
-import java.util*;
+import java.io.*;
+import java.util.*;
 
 public class MatrixThreats{
     public static void main(String[] args){
         boolean flag = false;
         int matrix_A[][],matrix_B[][],threats[][];
         String xvalues = "";
+        int length = 0;
         int [] values;
         String fName = "";
         String space= "\\s+";
         Scanner scan = new Scanner (System.in);
-        System.out.println("File name:");
+        System.out.println("File name: ");
         fName = scan.next();
         scan.close();
         try{
             BufferedReader buffer = new BufferedReader(new FileReader(fName));
             xvalues = buffer.readLine();
-            values = MatrixThreats.stringArrayToIntArray(xvalues.split(space));
+            String [] avalues = xvalues.split(" ");
+            length = avalues.length;
+            values = new int[length];
+            
+            for(int i = 0;i<length;i++){
+               values[i] = Integer.parseInt(avalues[i]);
+            }
             if(values[0]==values[3]){
                 flag = true;
             }
@@ -30,6 +37,8 @@ public class MatrixThreats{
                 return;
             }
             
+        }catch (IOException e) {
+         
         }
     }     
 }
